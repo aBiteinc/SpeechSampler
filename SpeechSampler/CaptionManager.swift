f1a4deb5b39935ef916b375db6b4c5fb35935b6f
Tomo <t.onaga.227@gmail.com>
2020-01-01 19:52:38 +0900

    日本語化

--- SpeechSampler/CaptionManager.swift
+++ SpeechSampler/CaptionManager.swift
@@ -15,7 +15,7 @@ final class CaptionManager: NSObject, ObservableObject {
   @Published var isEnabledRecordButton = false
   @Published var recordButtonText = ""
 
-  private let speechRecognizer = SFSpeechRecognizer(locale: Locale(identifier: "en-US"))!
+  private let speechRecognizer = SFSpeechRecognizer(locale: Locale(identifier: "ja-JP"))!
   private var recognitionRequest: SFSpeechAudioBufferRecognitionRequest?
   private var recognitionTask: SFSpeechRecognitionTask?
   private let audioEngine = AVAudioEngine()
@@ -30,14 +30,14 @@ final class CaptionManager: NSObject, ObservableObject {
         switch authStatus {
         case .authorized:
           self.isEnabledRecordButton = true
-          self.recordButtonText = "Start Recording"
+          self.recordButtonText = "音声認識を開始"
         case .denied:
           self.isEnabledRecordButton = false
-          self.recordButtonText = "User denied access to speech recognition"
+          self.recordButtonText = "音声認識への許可を与えてください"
 
         case .restricted:
           self.isEnabledRecordButton = false
-          self.recordButtonText = "Speech recognition restricted on this device"
+          self.recordButtonText = "この端末では音声認識が禁止されています"
 
         case .notDetermined:
           self.isEnabledRecordButton = false
@@ -45,7 +45,7 @@ final class CaptionManager: NSObject, ObservableObject {
 
         default:
           self.isEnabledRecordButton = false
-          self.recordButtonText = "Nothing"
+          self.recordButtonText = "なんでもないです"
         }
       }
     }
@@ -125,8 +125,8 @@ final class CaptionManager: NSObject, ObservableObject {
     try audioEngine.start()
 
     // Let the user know to start talking.
-    caption = "(Go ahead, I'm listening)"
-    recordButtonText = "Stop Recording"
+    caption = "（喋ってください。私は聞いてますよ！）"
+    recordButtonText = "音声認識を停止"
   }
 
   func switchRecording() {
@@ -150,7 +150,7 @@ extension CaptionManager: SFSpeechRecognizerDelegate {
   func speechRecognizer(_ speechRecognizer: SFSpeechRecognizer, availabilityDidChange available: Bool) {
     if available {
       isEnabledRecordButton = true
-      recordButtonText = "Start Recording"
+      recordButtonText = "音声認識を開始"
     } else {
       isEnabledRecordButton = false
       recordButtonText = "Recognition Not Available"
